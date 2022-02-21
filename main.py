0a5e156111588fe311699e7f3d2cd89c19318c1a
Максим Скризов <91967355+screaze@users.noreply.github.com>
2022-02-21 13:17:08 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,56 @@
+from requests import get, post
+from random import randint
+import pyfiglet
+alaala = 0
+alaala2 = 0
+alaala3 = 0
+print("[$] Чекер дискорд токенов V2")
+def variant1(token):
+    response = get('https://discord.com/api/v6/auth/login', headers={"Authorization": token})
+    return True if response.status_code == 200 else False
+
+def variant2(token):
+    response = post(f'https://discord.com/api/v6/invite/{randint(1,9999999)}', headers={'Authorization': token})
+    if "You need to verify your account in order to perform this action." in str(response.content) or "401: Unauthorized" in str(response.content):
+        alaala3 = alaala3+1
+        return False
+    else:
+        return True
+
+def variant2_Status(token):
+    response = post(f'https://discord.com/api/v6/invite/{randint(1,9999999)}', headers={'Authorization': token})
+    if response.status_code == 401:
+        return 'Invalid'
+    elif "You need to verify your account in order to perform this action." in str(response.content):
+        return 'Phone Lock'
+    else:
+        return 'Valid'
+
+if __name__ == "__main__":
+    try:
+        checked = []
+        with open('tokens.txt', 'r') as tokens:
+            for token in tokens.read().split('\n'):
+                if len(token) > 15 and token not in checked and variant2(token) == True:
+                    print(f'[LOG] Токен: {token} валидный!')
+                    alaala = alaala+1
+                    checked.append(token)
+                else:
+                    print(f'[LOG] Токен: {token} не валидный!')
+                    alaala2 = alaala2+1
+        if len(checked) > 0:
+            save = input(f'[LOG] {len(checked)} Валидных токенов! \n[!] Save to file? (y/n)').lower()
+            if save == 'y':
+                name = randint(100000000, 9999999999)
+                with open(f'{name}.txt', 'w') as saveFile:
+                    saveFile.write('\n'.join(checked))
+                print(f'[LOG] Токены были сохранены в {name}.txt!')
+        input('[$] Нажмите ENTER')
+        os.system("clear")
+        print("[LOG] Статистика:")
+        print("[!] Валид:", +alaala)
+        print("[!] Не валидные:", +alaala2)
+        print("[!] Не верифицированы по номеру телефона:", +alaala3)
+    except:
+        input('[LOG] Файл tokens.txt не найден! Нажмите ENTER для закрытия программы!')
+# End of code
