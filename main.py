2346e1a7db5bb847dbf8293cb01a42150fce14f8
Максим Скризов <91967355+screaze@users.noreply.github.com>
2022-02-27 19:03:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 from requests import get, post
 from random import randint
-import pyfiglet
 alaala = 0
 alaala2 = 0
 alaala3 = 0
 logo = """
 [SYSTEM] Discord Token Checker by Screaze
 """
-def variant1(token):
+def checker1(token):
     response = get('https://discord.com/api/v6/auth/login', headers={"Authorization": token})
     return True if response.status_code == 200 else False
 
-def variant2(token):
+def checker2(token):
     response = post(f'https://discord.com/api/v6/invite/{randint(1,9999999)}', headers={'Authorization': token})
     if "You need to verify your account in order to perform this action." in str(response.content) or "401: Unauthorized" in str(response.content):
         alaala3 = alaala3+1
@@ -19,7 +18,7 @@ def variant2(token):
     else:
         return True
 
-def variant2_Status(token):
+def locktokens(token):
     response = post(f'https://discord.com/api/v6/invite/{randint(1,9999999)}', headers={'Authorization': token})
     if response.status_code == 401:
         return 'Invalid'
@@ -33,7 +32,7 @@ if __name__ == "__main__":
         checked = []
         with open('tokens.txt', 'r') as tokens:
             for token in tokens.read().split('\n'):
-                if len(token) > 15 and token not in checked and variant2(token) == True:
+                if len(token) > 15 and token not in checked and checker2(token) == True:
                     print(f'[LOG] Токен: {token} валидный!')
                     alaala = alaala+1
                     checked.append(token)
